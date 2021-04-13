f14da3c629ee0db6d8a6dd7e88e732bb8203ab77
Terence Au <60273779+Takosheen@users.noreply.github.com>
2021-04-13 02:29:05 -0400

    dependencies, controller, dto added

--- SchoolAPI/Contracts/IOrganizationRepository.cs
+++ SchoolAPI/Contracts/IOrganizationRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities.Models;
 
 namespace Contracts
 {
     public interface IOrganizationRepository
     {
+        IEnumerable<Organization> GetAllOrganizations(bool trackChanges);
+        Organization GetOrganization(Guid companyId, bool trackChanges);
     }
 }
